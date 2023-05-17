11a9b88be3900a9ec001dab675a80979ac3b7dce
MuchamadAndriyanto <mandriyanto9@gmail.com>
2023-05-17 08:33:23 +0700

    Test Crash & Update Lint

--- app/src/main/java/com/example/bismillahjadi/MainActivity.kt
+++ app/src/main/java/com/example/bismillahjadi/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.bismillahjadi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
+import com.example.bismillahjadi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val crashButton = Button(this)
+        crashButton.text = "Test Crash"
+        crashButton.setOnClickListener {
+            throw RuntimeException("Test Crash") // Force a crash
+        }
+
+        addContentView(crashButton, ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT))
     }
 }
